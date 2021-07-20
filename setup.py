0b74f45372d5d6d007eeafe9527b2c0154a4d8fc
Daniel Dayley <daniel.dayley@sling.com>
2021-07-20 14:02:45 -0600

    Still working on systemd and launchd jobs

--- setup.py
+++ setup.py
@@ -3,42 +3,45 @@ from setuptools.command.install_scripts import install_scripts
 from setuptools.command.install import install
 from setuptools.command.develop import develop
 from setuptools.command.egg_info import egg_info
+from setuptools.command.build_ext import build_ext
 import subprocess
 import os
 import glob
 
 
 # From https://stackoverflow.com/questions/5932804/set-file-permissions-in-setup-py-file
+# https://blog.niteo.co/setuptools-run-custom-code-in-setup-py/
+def customize(command) :
+	command_name = str(command.mro()[1].__name__).strip()
+	original_run = command.run
+	def run(self) :
+		# Run the rest of the installer first
+		original_run(self)
+		# Create a new subprocess to run the included shell script
+		print("Running " + command_name + " commands...")
+		current_dir_path = os.path.dirname(os.path.realpath(__file__))
+		create_service_script_path = os.path.join(current_dir_path, 'setup.sh')
+		# stdout and stderr are combined in shell output
+		output=subprocess.run([create_service_script_path,command_name],stdout=subprocess.PIPE,stderr=subprocess.STDOUT).stdout
+		print(output.decode('UTF-8'))
+	command.run = run
+	return command
+
+@customize
 class CustomInstallCommand(install) :
-	def run(self) :
-		# Run the rest of the installer first
-		install.run(self)
-		# Create a new subprocess to run the included shell script
-		current_dir_path = os.path.dirname(os.path.realpath(__file__))
-		create_service_script_path = os.path.join(current_dir_path, 'install_systemd_service.sh')
-		subprocess.check_output([create_service_script_path])
+	pass
 
-
-
-# From https://stackoverflow.com/questions/5932804/set-file-permissions-in-setup-py-file
+@customize
 class CustomDevelopCommand(develop) :
-	def run(self) :
-		# Run the rest of the installer first
-		develop.run(self)
-		# Create a new subprocess to run the included shell script
-		current_dir_path = os.path.dirname(os.path.realpath(__file__))
-		create_service_script_path = os.path.join(current_dir_path, 'install_systemd_service.sh')
-		subprocess.check_output([create_service_script_path])
+	pass
 
-# From https://stackoverflow.com/questions/5932804/set-file-permissions-in-setup-py-file
+@customize
 class CustomEggInfoCommand(egg_info) :
-	def run(self) :
-		# Run the rest of the installer first
-		egg_info.run(self)
-		# Create a new subprocess to run the included shell script
-		current_dir_path = os.path.dirname(os.path.realpath(__file__))
-		create_service_script_path = os.path.join(current_dir_path, 'install_systemd_service.sh')
-		subprocess.check_output([create_service_script_path])
+	pass
+
+@customize
+class CustomBuildExtCommand(build_ext) :
+	pass
 
 files = glob.glob('python-tool/externals/*.py')
 
@@ -51,9 +54,9 @@ setup(name='python-tool',
 	description='',
 	packages=find_packages(exclude=['tests']),
 	package_data={"": ['externals/*.py']},
-	install_requires=['pyyaml', 'vsphere-automation-sdk @ git+https://github.com/vmware/vsphere-automation-sdk-python.git'],
+	install_requires=['pyyaml',],
 	scripts=['bin/python-tool'],
 	long_description=open('README.md').read(),
 	zip_safe=True,
-	cmdclass={'install':CustomInstallCommand,'develop':CustomDevelopCommand,'egg_info':CustomEggInfoCommand}
+	cmdclass={'install':CustomInstallCommand,'develop':CustomDevelopCommand,'egg_info':CustomEggInfoCommand,'build_ext':CustomBuildExtCommand}
 )
---
+++ setup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+COMMAND="$1"
+
+[ -z "$1" ] && echo "No setup command given, nothing to do." && exit 1
+
+export OS="$(uname -a)"
+[[ "$OS" == *"iPhone"* || "$OS" == *"iPad"* ]] && export OS="iOS"
+[[ "$OS" == *"ndroid"* ]] && export OS="Android"
+[[ "$OS" == *"kali"* ]] && export OS="Kali"
+[[ "$OS" == *"indows"* ]] && export OS="Windows"
+[[ "$OS" == *"arwin"* ]] && export OS="macOS"
+[[ "$OS" == *"BSD"* ]] && export OS="BSD"
+[[ "$OS" == *"inux"* ]] && export OS="Linux"
+
+setup_install() {
+	echo "Installing included service files..."
+	# Systemd job installation
+	if [[ "$OS" == "Linux" && $(pidof systemd) ]]; then
+		echo "Installing systemd job..."
+		SERVICE_FILE='python-tool.service'
+		SERVICE_DIR="/etc/systemd/system"
+		sudo cp -r "$SERVICE_FILE" "$SERVICE_DIR"/
+		sudo chown root:root "$SERVICE_DIR"/$"SERVICE_FILE"
+		sudo systemctl daemon-reload
+		sudo systemctl enable "$SERVICE_FILE"
+		echo "Installed systemd job."
+	fi
+
+	if [[ "$OS" == "macOS" && $(ps -p 1 | grep launchd) == *"launchd" ]]; then
+		echo "Installing launchd job..."
+		SERVICE_FILE='com.cronocide.python-tool.plist'
+		SERVICE_DIR="~/Library/LaunchAgents/"
+		cp -r "$SERVICE_FILE" "$SERVICE_DIR"/
+		sudo launchctl load "$SERVICE_FILE"
+		echo "Installed launchd job."
+	fi
+}
+
+setup_develop() {
+	echo "Not installing service files due to being installed in development mode."
+}
+
+setup_egg_info() {
+	echo "No custom egg metadata to update."
+}
+
+setup_build_ext() {
+	echo "No external build scripts called."
+}
+
+case "$COMMAND" in
+	"install")
+		setup_install;;
+	"develop")
+		setup_develop;;
+	"egg_info")
+		setup_egg_info;;
+	"build_ext")
+		setup_build_ext;;
+	*)
+esac
